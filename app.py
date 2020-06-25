fe61e936e659f0f6ef1144d7146cfd5df5a9b30d
Aaron Glover <aglove2189@gmail.com>
2020-06-25 13:36:16 -0500

    add daily numbers

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ def get_state_df(df, type):
     df = df.filter(regex="([0-9]+\/[0-9]+\/[0-9]+)|(Province_State)").rename(columns={"Province_State": "state"})
     df = df.melt(id_vars="state", var_name="date", value_name=f"total_{type}".lower())
     df["date"] = pd.to_datetime(df["date"])
-    return df.set_index("date").groupby("state").resample("D").sum().reset_index()
+    df = df.set_index("date").groupby("state").resample("D").sum().reset_index()
+    df[f"daily_{type}"] = df.groupby("state")[f"total_{type}"].diff().fillna(0).clip(lower=0)
+    return df
 
 
 def get_country_df(df, type):
@@ -26,7 +28,9 @@ def get_country_df(df, type):
     df["country"] = df["country"].replace({"US": "United States", "Korea, South": "South Korea"})
     df = df.melt(id_vars="country", var_name="date", value_name=f"total_{type}".lower())
     df["date"] = pd.to_datetime(df["date"])
-    return df.set_index("date").groupby("country").resample("D").sum().reset_index()
+    df = df.set_index("date").groupby("country").resample("D").sum().reset_index()
+    df[f"daily_{type}"] = df.groupby("country")[f"total_{type}"].diff().fillna(0).clip(lower=0)
+    return df
 
 
 def days_since(df, col, num=100, groupby="country"):
@@ -40,7 +44,7 @@ def chart(df, y, color="country"):
         .mark_line(point=True)
         .encode(
             x="days_since",
-            y=alt.Y(y, scale=alt.Scale(type="log", base=10)),
+            y=alt.Y(y),
             color=color,
             tooltip=[alt.Tooltip(color), alt.Tooltip(y, format=",")],
         )
@@ -48,7 +52,10 @@ def chart(df, y, color="country"):
     )
 
 
-def by_(by="country"):
+def main():
+    by = st.radio("Choose Analysis", ["Country", "State"])
+    by = by.lower()
+
     if by == "country":
         confirmed_df = get_df("confirmed", "global")
         deaths_df = get_df("deaths", "global")
@@ -56,28 +63,30 @@ def by_(by="country"):
         confirmed_df = get_df("confirmed", "US")
         deaths_df = get_df("deaths", "US")
 
+    chart_type = st.radio("Choose Chart Type", ["Cumulative", "Daily"])
+
     num_confirmed = st.text_input("Number of Confirmed:", 100)
     confirmed_since_df = days_since(
         confirmed_df, "total_confirmed", num=int(num_confirmed), groupby=by
     )
 
-    top_10 = (
+    top_5 = (
         confirmed_since_df.groupby(by)["total_confirmed"]
         .max()
         .sort_values(ascending=False)
-        .head(10)
+        .head(5)
         .index.tolist()
     )
 
     select_all = confirmed_since_df[by].unique().tolist()
     if by == "state":
-        selection = ["Top 10", "Select All"]
+        selection = ["Top 5", "Select All"]
     else:
-        selection = ["Top 10", "US vs Italy vs South Korea", "Select All"]
+        selection = ["Top 5", "US vs Italy vs South Korea", "Select All"]
     radio = st.radio("", selection)
 
-    if radio == "Top 10":
-        multi = st.multiselect("", select_all, default=top_10)
+    if radio == "Top 5":
+        multi = st.multiselect("", select_all, default=top_5)
     elif radio == "Select All":
         multi = st.multiselect("", select_all, default=select_all)
     elif radio == "US vs Italy vs South Korea":
@@ -87,14 +96,20 @@ def by_(by="country"):
     confirmed_since_df = confirmed_since_df[confirmed_since_df[by].isin(multi)]
 
     st.markdown(f"## Confirmed cases by days since {num_confirmed} confirmed 😷")
-    st.altair_chart(chart(confirmed_since_df, "total_confirmed", color=by))
+    if chart_type == "Cumulative":
+        st.altair_chart(chart(confirmed_since_df, "total_confirmed", color=by))
+    else:
+        st.altair_chart(chart(confirmed_since_df, "daily_confirmed", color=by))
 
     num_deaths = st.text_input("Number of Death(s):", 1)
     deaths_since_df = days_since(deaths_df, "total_deaths", num=int(num_deaths), groupby=by)
     deaths_since_df = deaths_since_df[deaths_since_df[by].isin(multi)]
 
     st.markdown(f"## Deaths by days since {num_deaths} death(s)")
-    st.altair_chart(chart(deaths_since_df, "total_deaths", color=by))
+    if chart_type == "Cumulative":
+        st.altair_chart(chart(deaths_since_df, "total_deaths", color=by))
+    else:
+        st.altair_chart(chart(deaths_since_df, "daily_deaths", color=by))
 
     st.markdown("## Totals")
     df = (
@@ -114,6 +129,4 @@ def by_(by="country"):
 
 if __name__ == "__main__":
     st.title("COVID-19 🦠")
-
-    analysis = st.sidebar.selectbox("Choose Analysis", ["Country", "State"])
-    by_(analysis.lower())
+    main()
